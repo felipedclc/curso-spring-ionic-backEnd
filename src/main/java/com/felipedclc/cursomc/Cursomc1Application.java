a0fc085af3a4137273f1f8b4d6f9a85317174fd0
Felipe Cardoso <felipedclc@gmail.com>
2020-07-23 12:31:23 -0300

    Criando o profile dev(sem uso) - duvida

--- src/main/java/com/felipedclc/cursomc/Cursomc1Application.java
+++ src/main/java/com/felipedclc/cursomc/Cursomc1Application.java
@@ -1,8 +1,15 @@
 package com.felipedclc.cursomc;
 
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.felipedclc.cursomc.services.DBService;
 
 @SpringBootApplication
 public class Cursomc1Application implements CommandLineRunner {  
@@ -10,7 +17,22 @@ public class Cursomc1Application implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(Cursomc1Application.class, args);
 	}
+	
+	@Autowired
+	private DBService dbService;
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
 
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+
+		dbService.instantiateTestDatabase();
+		return true;
+	}
 	@Override
 	public void run(String... args) throws Exception {
 	}
