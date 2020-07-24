a08e8aa294c21a652efba63b7a75f9383a704530
Felipe Cardoso <felipedclc@gmail.com>
2020-07-24 18:03:10 -0300

    Correcao profile dev, outras correcoes

--- src/main/java/com/felipedclc/cursomc/config/DevConfig.java
+++ src/main/java/com/felipedclc/cursomc/config/DevConfig.java
@@ -3,7 +3,6 @@ package com.felipedclc.cursomc.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -17,15 +16,8 @@ public class DevConfig { // CONFIGURAÇÕES ESPECÍFICA DO PROFILE DEV (applicat
 	@Autowired
 	private DBService dbService;
 	
-	@Value("${spring.jpa.hibernate.ddl-auto}") // PEGANDO O VALOR DA CHAVE
-	private String strategy; // ESTRATEGIA DE GERAÇÃO DO BD
-	
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
-		
-		if("create" != strategy) { 
-			return false; 
-		}
 		
 		dbService.instantiateTestDatabase();
 		return true;
--- src/main/java/com/felipedclc/cursomc/services/DBService.java
+++ src/main/java/com/felipedclc/cursomc/services/DBService.java
@@ -54,8 +54,7 @@ public class DBService {
 	
 	
 	public void instantiateTestDatabase() throws ParseException {
-		
-		
+				
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		Categoria cat3 = new Categoria(null, "Cama mesa e banho");
